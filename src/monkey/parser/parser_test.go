17f8d04c68c52ac2cadff1297e3f83a52bda949c
moulindavid <moulin.david@live.fr>
2023-06-24 16:52:34 +0200

    feat: parser minimal error handling

--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -15,6 +15,7 @@ let foobar🙃 = 838383;
 	l := lexer.New(input)
 	p := New(l)
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -36,6 +37,7 @@ let foobar🙃 = 838383;
 		}
 	}
 }
+
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	if s.TokenLiteral() != "let" {
 		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
@@ -56,4 +58,16 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 		return false
 	}
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
