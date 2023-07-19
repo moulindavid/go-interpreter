925ecef7a60a553df6f2d748d6666dfdc43a4480
moulindavid <moulin.david@live.fr>
2023-07-19 18:42:49 +0200

    feat: parse int and use go mod

--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -126,3 +126,51 @@ func TestIdentifierExpression(t *testing.T) {
 			ident.TokenLiteral())
 	}
 }
+
+func maxProfit(prices []int) int {
+	max := 0
+	left, right := 0, 1
+
+	for right < len(prices) {
+
+		profit := prices[right] - prices[left]
+
+		if profit > 0 {
+			if max < profit {
+				max = profit
+			}
+		} else {
+			left++
+		}
+		right++
+	}
+	return max
+}
+
+func TestIntegerLiteralExpression(t *testing.T) {
+	input := "5;"
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d",
+			len(program.Statements))
+	}
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("exp not *ast.IntegerLiteral. got=%T", stmt.Expression)
+	}
+	if literal.Value != 5 {
+		t.Errorf("literal.Value not %d. got=%d", 5, literal.Value)
+	}
+	if literal.TokenLiteral() != "5" {
+		t.Errorf("literal.TokenLiteral not %s. got=%s", "5",
+			literal.TokenLiteral())
+	}
+}
